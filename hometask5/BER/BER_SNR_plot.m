99efaa9cb17ea9a6cd02b50df95a46fdcfd64b3f
SofyaSavelyeva <89632493+sofyafyaa@users.noreply.github.com>
2023-05-13 14:09:55 +0300

    Delete hometask5 directory

--- hometask5/BER/BER_SNR_plot.m
+++
@@ -1,9 +0,0 @@
-function [Ber_i,outputArg2] = BER_SNR_plot(, Constellation)
-%BER_SNR_PLOT Summary of this function goes here
-%   Detailed explanation goes here
-
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
